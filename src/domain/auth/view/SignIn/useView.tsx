23cf2f5ca6362df7820243351ec4e31ac39e71ac
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-17 08:27:10 -0300

    feat(auth domain): add validation on signin form

--- src/domain/auth/view/SignIn/useView.tsx
+++ src/domain/auth/view/SignIn/useView.tsx
@@ -1,10 +1,23 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import validationErrors from '../../../../lib/errors/validation'
 import useAuth from '../../hooks/useAuth'
 import { SignInValues } from '../../types'
 import { SignInView } from '../../types/view/SignIn/view'
 
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email(validationErrors.emailIsInvalid)
+    .required(validationErrors.emailIsRequired),
+  password: yup.string().required(validationErrors.passwordIsRequired),
+})
+
 export default function useSignInView(): SignInView {
-  const form = useForm<SignInValues>()
+  const form = useForm<SignInValues>({
+    resolver: yupResolver(validationSchema),
+  })
   const { signIn } = useAuth()
   const handleSubmit = form.handleSubmit(async (values: SignInValues): Promise<void> => {
     await signIn(values)
---
+++ src/lib/errors/validation.ts
@@ -0,0 +1,8 @@
+const validationErrors = {
+    emailIsRequired: 'Por favor, informe um e-mail',
+    emailIsInvalid: 'Por favor, informe um e-mail válido',
+    passwordIsRequired: 'Por favor, informe uma senha',
+    passwordIsInvalid: 'Por favor, informe uma senha válida',
+}
+
+export default validationErrors
