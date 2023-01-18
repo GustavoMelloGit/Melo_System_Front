f93c3337168a4d77a86ecacc7bed53790194fd65
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-18 19:54:18 -0300

    feat(client domain): create client model

---
+++ src/domain/client/types/model/Client.ts
@@ -0,0 +1,32 @@
+export type ClientModel = {
+    id: string;
+    profileImage: string;
+    name: string;
+    nickname: string;
+    phone: string;
+    balance: number;
+    personType: 'fisica' | 'juridica';
+    cpf: string;
+    rg: string;
+    rgEmissionDate: string;
+    producerRegistration: string;
+    cnpj: string;
+    stateRegistration: string;
+    birthDate: string;
+    fatherName: string;
+    motherName: string;
+    address: ClientAddressModel;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export type ClientAddressModel = {
+    city: string;
+    zipCode: string;
+    street: string;
+    number: string;
+    state: string;
+    neighborhood: string;
+    brook: string;
+}
