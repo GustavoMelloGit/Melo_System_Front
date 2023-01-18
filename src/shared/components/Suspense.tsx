9d66df8f66f3f3664512d120ab7db273670c765d
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-18 20:22:00 -0300

    fix: inverse toggle sidebar button icon

--- src/shared/components/Suspense.tsx
+++ src/shared/components/Suspense.tsx
@@ -1,5 +1,14 @@
+import { Center, Heading } from '@chakra-ui/react'
 import React, { PropsWithChildren } from 'react'
 
 export default function Suspense({ children }: PropsWithChildren): JSX.Element {
-  return <React.Suspense fallback={<div>Loading...</div>}>{children}</React.Suspense>
+  return <React.Suspense fallback={<SuspenseLoader />}>{children}</React.Suspense>
+}
+
+function SuspenseLoader(): JSX.Element {
+  return (
+    <Center minH='100vh' minW='100vw'>
+      <Heading as='h1'>Carregando...</Heading>
+    </Center>
+  )
 }
