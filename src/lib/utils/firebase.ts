266b7d1f415b3d467f1f1f673469c735aa9bffa2
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-18 22:48:01 -0300

    feat: add query into useSwrFirebase hook

--- src/lib/utils/firebase.ts
+++ src/lib/utils/firebase.ts
@@ -1,16 +1,32 @@
-import { collection, DocumentData, getDocs } from "firebase/firestore"
+import { collection, CollectionReference, DocumentData, getDocs, query, QueryConstraint } from "firebase/firestore"
 import useSWR from "swr"
 import { UseSwrFirebase } from "../../shared/types/utils/firebase"
 import { firestore } from "../config/firebase"
 
-export async function firebaseFetcher(collectionName: string): Promise<DocumentData[]> {
-    const querySnapshot = await getDocs(collection(firestore, collectionName))
+
+export async function firebaseQueryBuilder(
+    collectionRef: CollectionReference<DocumentData>,
+    ...queryConstraints: QueryConstraint[]
+): Promise<DocumentData[]> {
+    const q = query(collectionRef, ...queryConstraints)
+    const querySnapshot = await getDocs(q)
     const data = querySnapshot.docs.map((doc) => doc.data())
     return data
 }
 
-export function useSwrFirebase<T>(collectionName: string): UseSwrFirebase<T> {
-    const { data, error } = useSWR<DocumentData[], Error>(collectionName, firebaseFetcher)
+export async function firebaseFetcher(collectionName: string, ...queryConstraints: QueryConstraint[]): Promise<DocumentData[]> {
+    const collectionRef = collection(firestore, collectionName)
+    const data = await firebaseQueryBuilder(collectionRef, ...queryConstraints)
+    return data
+}
+
+export function useSwrFirebase<T>(collectionName: string, ...queryConstraints: QueryConstraint[]): UseSwrFirebase<T> {
+    const { data, error } = useSWR<DocumentData[], Error>(
+        collectionName,
+        async () => firebaseFetcher(collectionName, ...queryConstraints)
+    )
+
+    console.log(error)
     return {
         data: data as T | undefined,
         isLoading: !error && !data,
