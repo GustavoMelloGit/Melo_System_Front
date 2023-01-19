91c6ceb1b380967be29c9fab15cf9d2000d6989f
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-18 22:02:58 -0300

    refactor: move ServiceResponse type to types folder

--- src/domain/auth/types/service/index.ts
+++ src/domain/auth/types/service/index.ts
@@ -1,4 +1,4 @@
-import { ServiceResponse } from "../../../../lib/utils/service";
+import { ServiceResponse } from "../../../../shared/types/utils/service";
 import { AuthContextType } from "../context/auth";
 
 export type SignInResponse = ServiceResponse<AuthContextType['user']>
