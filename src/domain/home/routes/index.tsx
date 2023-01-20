22fe407888f40f699119645e2d135ee707724b7d
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-20 10:05:39 -0300

    feat: create mock charts into dashboard

--- src/domain/home/routes/index.tsx
+++ src/domain/home/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { RouteObject } from 'react-router-dom'
 import { Routes } from '../../../lib/routes'
 
-const HomeView = React.lazy(async () => import('../view'))
+const HomeView = React.lazy(async () => import('../view/Dashboard'))
 
 const homeRoutes: RouteObject[] = [
   {
