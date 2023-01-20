dcb497fd1db999a4c4d813bc90d14c887b5813a9
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-19 22:30:42 -0300

    style(layout): center color mode switch

--- src/shared/components/layout/Sidebar/components/SidebarFooter.tsx
+++ src/shared/components/layout/Sidebar/components/SidebarFooter.tsx
@@ -1,15 +1,15 @@
-import { Box, Flex, Switch, useColorMode } from '@chakra-ui/react'
+import { Flex, Switch, useColorMode } from '@chakra-ui/react'
 import { FiMoon, FiSun } from 'react-icons/fi'
 
 export default function SidebarFooter(): JSX.Element {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <Box as='footer' justifySelf='flex-end' pb={4}>
+    <Flex justify='center' as='footer' justifySelf='flex-end' pb={4}>
       <Flex gap={2} align='center'>
         <FiSun size={22} />
         <Switch onChange={toggleColorMode} isChecked={colorMode === 'dark'} />
         <FiMoon size={22} />
       </Flex>
-    </Box>
+    </Flex>
   )
 }
