1224c1689b267770dd83afe8eee9876a892922a1
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-01-19 22:49:50 -0300

    feat: add first chart at home page

--- src/domain/home/view/index.tsx
+++ src/domain/home/view/index.tsx
@@ -1,12 +1,34 @@
-import { Box, Button, useColorMode } from '@chakra-ui/react'
+import { Card, Heading, useColorModeValue, VStack } from '@chakra-ui/react'
+import { ApexOptions } from 'apexcharts'
+import Chart from 'react-apexcharts'
 
 export default function HomePage(): JSX.Element {
-  const { toggleColorMode } = useColorMode()
+  const tooltipTheme = useColorModeValue('light', 'dark')
+  const chartOptions: ApexOptions = {
+    chart: {
+      id: 'basic-bar',
+    },
+    xaxis: {
+      categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
+    },
+    tooltip: {
+      theme: tooltipTheme,
+    },
+  }
+
+  const series = [
+    {
+      name: 'series-1',
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+  ]
+
   return (
-    <Box>
-      <Box>oi</Box>
-      <Button onClick={toggleColorMode}>Toggle color</Button>
-      <Box />
-    </Box>
+    <VStack align='flex-start' spacing={10}>
+      <Heading>Página inicial</Heading>
+      <Card pt={5} pr={5}>
+        <Chart options={chartOptions} series={series} height={200} width={400} type='line' />
+      </Card>
+    </VStack>
   )
 }
