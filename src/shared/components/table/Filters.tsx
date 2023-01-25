d16cd8cdb26b84a6a167d8c339d667cd906802fc
Gustavo Mello <gugamello2014@yahoo.com>
2023-01-25 19:35:49 -0300

    Revert "feat(test): sdd"
    This reverts commit f24fcf14a11f15e1539b09e834d5bb9b5e4da0ca.

--- src/shared/components/table/Filters.tsx
+++ src/shared/components/table/Filters.tsx
@@ -1,9 +1,9 @@
-import { Box, Input } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 export default function TableFilters(): JSX.Element {
   return (
     <Box>
-      <Input />
+      <p>Hello World</p>
     </Box>
   )
 }
