7b1b8c9d783cd8514e6f7bf42409fa083bdf066a
Gustavo Mello <gugamello2014@yahoo.com>
2023-01-26 19:26:04 -0300

    build: change app to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import { HelmetProvider } from 'react-helmet-async'
-import { Toaster } from 'react-hot-toast'
-import { RouterProvider } from 'react-router-dom'
-import { AuthProvider } from './domain/auth/context/AuthContext'
-import './lib/config/firebase'
-import router from './lib/routes/router'
-import theme, { themeManager } from './lib/styles/theme'
-import ProviderComposer from './shared/components/ProviderComposer'
-import Suspense from './shared/components/Suspense'
-import LayoutProvider from './shared/contexts/LayoutContext'
-function App(): JSX.Element {
-  return (
-    <>
-      <ChakraProvider theme={theme} colorModeManager={themeManager}>
-        <ColorModeScript
-          storageKey='@melo-system:theme'
-          initialColorMode={theme.config.initialColorMode}
-        />
-        <ProviderComposer contexts={[AuthProvider, LayoutProvider, Suspense, HelmetProvider]}>
-          <RouterProvider router={router} />
-        </ProviderComposer>
-      </ChakraProvider>
-      <Toaster />
-    </>
-  )
-}
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
+import { AuthProvider } from './domain/auth/context/AuthContext';
+import router from './lib/routes/router';
+import theme, { themeManager } from './lib/styles/theme';
+import ProviderComposer from './shared/components/ProviderComposer';
+import Suspense from './shared/components/Suspense';
+import LayoutProvider from './shared/contexts/LayoutContext';
 
-export default App
+function App() {
+  return (
+    <ChakraProvider theme={theme} colorModeManager={themeManager}>
+      <ColorModeScript
+        storageKey='@melo-system:theme'
+        initialColorMode={theme.config.initialColorMode}
+      />
+      <ProviderComposer
+        contexts={[LayoutProvider, Suspense, HelmetProvider, AuthProvider]}
+      >
+        <RouterProvider router={router} />
+      </ProviderComposer>
+      <Toaster />
+    </ChakraProvider>
+  );
+}
+export default App;
