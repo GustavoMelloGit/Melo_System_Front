a4815dfbefb0f80f62495055718e3cb8720db67f
Gustavo Mello <gugamello2014@yahoo.com>
2023-01-26 20:49:32 -0300

    style(shared): suspense component changing bg color based on color mode

--- src/shared/components/Suspense.tsx
+++ src/shared/components/Suspense.tsx
@@ -1,4 +1,4 @@
-import { Center } from '@chakra-ui/react'
+import { Center, useColorModeValue } from '@chakra-ui/react'
 import React, { type PropsWithChildren } from 'react'
 import SpinLoader from './SpinLoader'
 
@@ -7,9 +7,10 @@ export default function Suspense({ children }: PropsWithChildren): JSX.Element {
 }
 
 export function SuspenseLoader(): JSX.Element {
+  const bg = useColorModeValue('gray.100', 'gray.700')
   return (
     <Center flex={1}>
-      <Center boxSize={120} bg='gray.700' rounded={25} shadow='xl'>
+      <Center boxSize={120} bg={bg} rounded={25} shadow='xl'>
         <SpinLoader size='lg' />
       </Center>
     </Center>
