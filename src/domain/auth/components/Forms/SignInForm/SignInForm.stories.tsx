70ae4866ec44021d744baa5b213fc13b1624aff7
Gustavo Mello <gugamello2014@yahoo.com>
2023-01-27 20:04:27 -0300

    test(auth domain): create SignIn form story

---
+++ src/domain/auth/components/Forms/SignInForm/SignInForm.stories.tsx
@@ -0,0 +1,9 @@
+import { type ComponentMeta, type StoryObj } from '@storybook/react'
+import SignInForm from '.'
+
+export default {
+  title: 'Auth/SignInForm',
+  component: SignInForm,
+} as ComponentMeta<typeof SignInForm>
+
+export const Default: StoryObj = {}
