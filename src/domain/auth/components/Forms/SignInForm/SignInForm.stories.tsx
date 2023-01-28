b573b7f395ec23baae067d16ac34573196c6eef1
Gustavo Mello <gugamello2014@yahoo.com>
2023-01-27 23:28:10 -0300

    test(auth domain): add auth form Filled and Filled Error stories

--- src/domain/auth/components/Forms/SignInForm/SignInForm.stories.tsx
+++ src/domain/auth/components/Forms/SignInForm/SignInForm.stories.tsx
@@ -1,9 +1,60 @@
+import { Center } from '@chakra-ui/react'
 import { type ComponentMeta, type StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
 import SignInForm from '.'
 
 export default {
   title: 'Auth/SignInForm',
   component: SignInForm,
+  decorators: [
+    (Story) => (
+      <Center>
+        <Story />
+      </Center>
+    ),
+  ],
 } as ComponentMeta<typeof SignInForm>
 
 export const Default: StoryObj = {}
+
+export const Filled: StoryObj = {}
+Filled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+
+  const emailInput = canvas.getByRole('textbox', { name: /email/i })
+
+  await userEvent.type(emailInput, 'test@test.com', {
+    delay: 100,
+  })
+
+  const passwordInput = canvas.getByLabelText(/senha/i)
+
+  await userEvent.type(passwordInput, '123456', {
+    delay: 100,
+  })
+
+  const button = canvas.getByRole('button', { name: /login/i })
+
+  userEvent.click(button)
+}
+
+export const FilledError: StoryObj = {}
+FilledError.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+
+  const emailInput = canvas.getByRole('textbox', { name: /email/i })
+
+  await userEvent.type(emailInput, 'invalid-email', {
+    delay: 100,
+  })
+
+  const passwordInput = canvas.getByLabelText(/senha/i)
+
+  await userEvent.type(passwordInput, '123456', {
+    delay: 100,
+  })
+
+  const button = canvas.getByRole('button', { name: /login/i })
+
+  userEvent.click(button)
+}
