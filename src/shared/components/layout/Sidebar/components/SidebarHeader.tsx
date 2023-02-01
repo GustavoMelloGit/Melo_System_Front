b0d3c2b6a66c08b5d443f6fc8baacd0e621e8f7b
Gustavo Mello <gugamello2014@yahoo.com>
2023-02-01 19:52:15 -0300

    fix(shared): sidebar header uses current logged in user

--- src/shared/components/layout/Sidebar/components/SidebarHeader.tsx
+++ src/shared/components/layout/Sidebar/components/SidebarHeader.tsx
@@ -5,7 +5,7 @@ import useLayoutContext from '../../../../hooks/useLayoutContext'
 import ToggleSidebarButton from '../../../buttons/ToggleSidebarButton'
 
 export default function SidebarHeader(): JSX.Element {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
   const {
     sidebar: { isOpen },
   } = useLayoutContext()
@@ -13,10 +13,10 @@ export default function SidebarHeader(): JSX.Element {
     <VStack align='stretch' as='header'>
       {isOpen && <ToggleSidebarButton alignSelf='flex-end' />}
       <Flex align='center' gap={4}>
-        <Avatar name='Test' />
+        <Avatar name={user.email} />
         <VStack align='flex-start' w='full'>
           <Heading noOfLines={1} as='h1' fontSize='md'>
-            test@test.com
+            {user.email}
           </Heading>
           <Button variant='outline' size='sm' onClick={signOut} gap={2} lineHeight={0.5}>
             <GoSignOut />
