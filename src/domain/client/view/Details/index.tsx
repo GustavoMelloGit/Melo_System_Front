a02f0356c5445ec8c845511e87e3b6a6d2386e17
Gustavo Mello <gugamello2014@yahoo.com>
2023-02-06 21:13:59 -0300

    feat(client domain): add tabs to detail page

--- src/domain/client/view/Details/index.tsx
+++ src/domain/client/view/Details/index.tsx
@@ -1,4 +1,22 @@
-import { Avatar, Card, CardHeader, Heading, HStack, VStack } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Flex,
+  Heading,
+  Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tag,
+  TagLeftIcon,
+} from '@chakra-ui/react'
+import { IoWarningOutline } from 'react-icons/io5'
 import { Routes } from '../../../../lib/routes'
 import HeaderBreadcrumbs from '../../../../shared/components/layout/Header/HeaderBreadcrumbs'
 import Page from '../../../../shared/components/Page'
@@ -25,17 +43,69 @@ export default function ClientDetails(): JSX.Element {
       />
       <Card>
         <CardHeader>
-          <HStack spacing={4}>
+          <Stack flexDir={['column', 'row']} align={'center'} gap={8}>
             <Avatar size='2xl' loading='lazy' src={client.profileImage} name={client.name} />
-            <VStack align='flex-start'>
-              <Heading as='h1'>{client.name}</Heading>
-              <Heading size='md' fontWeight={500}>
-                {client.nickname}
+            <Box textAlign={['center', 'left']}>
+              <Heading as='h1' fontSize={['xl', '4xl']}>
+                {client.name}
               </Heading>
-            </VStack>
-          </HStack>
+              <Heading size={['sm', 'md']} fontWeight={400} fontStyle='italic'>
+                ({client.nickname ?? 'Sem apelido'})
+              </Heading>
+            </Box>
+          </Stack>
         </CardHeader>
+        <CardBody pt={0}>
+          <Flex justify='center'>
+            <Tabs isLazy w='full'>
+              <Box overflow='auto'>
+                <TabList justifyContent='center' minW='max-content' w='full'>
+                  <Tab>Conta Corrente</Tab>
+                  <Tab>Conta Café</Tab>
+                  <Tab>Conta Escolha</Tab>
+                  <Tab>Conta Colheita</Tab>
+                  <Tab>Conta Sacaria</Tab>
+                  <Tab>Informações Gerais</Tab>
+                </TabList>
+              </Box>
+              <TabPanels>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+                <TabPanel>
+                  <InDevelopmentTag />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Flex>
+        </CardBody>
       </Card>
     </Page>
+  )
+}
+
+// TODO - Remove this component once the tabs are implemented
+function InDevelopmentTag(): JSX.Element {
+  return (
+    <Center>
+      <Tag colorScheme='yellow'>
+        <TagLeftIcon>
+          <IoWarningOutline size={26} />
+        </TagLeftIcon>
+        Em desenvolvimento
+      </Tag>
+    </Center>
   )
 }
