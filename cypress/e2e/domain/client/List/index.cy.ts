7f5f5609f849fb4ce3f9b52d595af7a93efbb76e
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-03-02 19:14:26 -0300

    test(client domain): create tests for search on clients table

--- cypress/e2e/domain/client/List/index.cy.ts
+++ cypress/e2e/domain/client/List/index.cy.ts
@@ -1,4 +1,4 @@
-import { PaginationParams } from '../../../../../src/lib/constants/pagination'
+import { ClientModel } from '../../../../../src/domain/client/types/model/Client'
 import { Routes } from '../../../../../src/lib/routes'
 
 const defaultHiddenBalance = 'R$ ----'
@@ -11,7 +11,6 @@ describe('Client Domain - List View', () => {
       method: 'GET',
       url: '/clients?*',
     }).as('getClients')
-    cy.wait('@getClients')
   })
 
   it('should render page', () => {
@@ -36,34 +35,26 @@ describe('Client Domain - List View', () => {
     cy.dataCy('table-cell-client-balance').first().click()
     cy.dataCy('table-cell-client-balance').first().should('have.text', defaultHiddenBalance)
   })
-  it('should be able to sort by name', () => {
+  it.only('should be able to sort by name', () => {
+    cy.wait('@getClients')
     cy.dataCy('table-header-name').within(() => {
       cy.dataCy('table-sort-button').click()
-    })
-    cy.location().should((loc) => {
-      expect(loc.search).to.include(
-        `${PaginationParams.sortBy}=name&${PaginationParams.sortOrder}=`,
-      )
     })
     cy.intercept({
       method: 'GET',
       url: '/clients?*',
-    }).as('getClients')
-    cy.wait('@getClients').then((interception) => {
-      const clients = interception.response.body.data
+    }).as('sortedGetClients')
+    cy.wait('@sortedGetClients').then((interception) => {
+      const clients = interception.response.body.data as ClientModel[]
       const clientsName = clients.map((client) => client.name).filter(Boolean) as string[]
       const sortedClientsName = [...clientsName].sort().reverse()
+      console.log(clientsName, sortedClientsName)
       expect(clientsName).to.deep.equal(sortedClientsName)
     })
   })
   it('should be able to sort by nickname', () => {
     cy.dataCy('table-header-nickname').within(() => {
       cy.dataCy('table-sort-button').click()
-    })
-    cy.location().should((loc) => {
-      expect(loc.search).to.include(
-        `${PaginationParams.sortBy}=nickname&${PaginationParams.sortOrder}=desc`,
-      )
     })
     cy.intercept({
       method: 'GET',
@@ -80,20 +71,76 @@ describe('Client Domain - List View', () => {
     cy.dataCy('table-header-balance').within(() => {
       cy.dataCy('table-sort-button').click()
     })
-    cy.location().should((loc) => {
-      expect(loc.search).to.include(
-        `${PaginationParams.sortBy}=balance&${PaginationParams.sortOrder}=desc`,
-      )
-    })
     cy.intercept({
       method: 'GET',
       url: '/clients?*',
     }).as('getClients')
     cy.wait('@getClients').then((interception) => {
-      const clients = interception.response.body.data
+      const clients = interception.response.body.data as ClientModel[]
       const clientsBalance = clients.map((client) => client.balance).filter(Boolean) as number[]
       const sortedClientsBalance = [...clientsBalance].sort().reverse()
       expect(clientsBalance).to.deep.equal(sortedClientsBalance)
+    })
+  })
+  it('should be able to search by name', () => {
+    cy.dataCy('table-cell-client-name')
+      .first()
+      .invoke('text')
+      .then((clientName) => {
+        cy.dataCy('table-searchFor-select').select('name')
+        cy.dataCy('table-search-input').type(clientName)
+        cy.dataCy('table-submit-search-button').click()
+        cy.intercept({
+          method: 'GET',
+          url: '/clients?*',
+        }).as('getClients')
+        cy.wait('@getClients').then((interception) => {
+          const clients = interception.response.body.data as ClientModel[]
+          const clientsName = clients.map((client) => client.name).filter(Boolean) as string[]
+          expect(clientsName).to.include(clientName)
+        })
+      })
+  })
+  it('should be able to search by nickname', () => {
+    cy.dataCy('table-cell-client-nickname')
+      .first()
+      .invoke('text')
+      .then((clientNickname) => {
+        cy.dataCy('table-searchFor-select').select('nickname')
+        cy.dataCy('table-search-input').type(clientNickname)
+        cy.dataCy('table-submit-search-button').click()
+        cy.intercept({
+          method: 'GET',
+          url: '/clients?*',
+        }).as('getClients')
+        cy.wait('@getClients').then((interception) => {
+          const clients = interception.response.body.data as ClientModel[]
+          const clientsNickname = clients
+            .map((client) => client.nickname)
+            .filter(Boolean) as string[]
+          expect(clientsNickname).to.include(clientNickname)
+        })
+      })
+  })
+  it('should be able to search by brook', () => {
+    const brookSearchFor = 'address.brook'
+    cy.wait('@getClients').then((interception) => {
+      const clients = interception.response.body.data as ClientModel[]
+      const clientBrook = clients[0].address.brook ?? ''
+      cy.dataCy('table-searchFor-select').select(brookSearchFor)
+      cy.dataCy('table-search-input').type(clientBrook)
+      cy.dataCy('table-submit-search-button').click()
+      cy.intercept({
+        method: 'GET',
+        url: '/clients?*',
+      }).as('getClients')
+      cy.wait('@getClients').then((interception) => {
+        const clients = interception.response.body.data as ClientModel[]
+        const clientsBrook = clients
+          .map((client) => client.address.brook)
+          .filter(Boolean) as string[]
+        expect(clientsBrook).to.include(clientBrook)
+      })
     })
   })
 })
