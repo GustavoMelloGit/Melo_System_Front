28aaca17d7898c5d152ca0e286d9937f67fe5fd8
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-03-11 09:20:35 -0300

    test(coffee domain): create update pickup tests

---
+++ cypress/e2e/domain/coffee/Pickup/update.cy.ts
@@ -0,0 +1,55 @@
+import { Routes } from '../../../../../src/lib/routes'
+import {
+  pickupFormRequiredFields,
+  pickupFormRequiredFieldsValues,
+} from '../../../../support/constants/Coffee/pickup'
+
+describe('Pickup Coffee - Update', () => {
+  let formData: { clientName: string; bags: string; address: string }
+  beforeEach(() => {
+    cy.login()
+    cy.visit(Routes.coffeePickups)
+    cy.dataCy('pickupCoffee-edit').first().click()
+    cy.dataCy('pickupCoffee-form').should('exist')
+    cy.fixture('Pickup/form.json').then((form) => {
+      formData = form
+    })
+  })
+
+  it('should be able to update a pickup', () => {
+    cy.dataCy('pickupCoffee-form').within(() => {
+      pickupFormRequiredFields.forEach((field) => {
+        cy.dataCy(field).clear().type(pickupFormRequiredFieldsValues.get(field))
+      })
+      cy.dataCy('submit-pickupCoffee-button').click()
+    })
+    cy.dataCy('pickupCoffee-form').should('not.exist')
+    cy.dataCy('pickupCoffee-table').should('exist')
+    cy.get('.toaster-success').should('exist')
+  })
+  it('should be able to cancel update', () => {
+    cy.dataCy('close-modal-button').click()
+    cy.dataCy('pickupCoffee-form').should('not.exist')
+    cy.dataCy('pickupCoffee-table').should('exist')
+    cy.get('.toaster-success').should('not.exist')
+  })
+  it('should be able to cancel update with esc', () => {
+    cy.get('body').type('{esc}')
+    cy.dataCy('pickupCoffee-form').should('not.exist')
+    cy.dataCy('pickupCoffee-table').should('exist')
+    cy.get('.toaster-success').should('not.exist')
+  })
+  it('should not be able to update a pickup without required fields', () => {
+    cy.dataCy('pickupCoffee-form').within(() => {
+      pickupFormRequiredFields.forEach((field) => {
+        cy.dataCy(field).clear()
+      })
+      cy.dataCy('submit-pickupCoffee-button').click()
+      pickupFormRequiredFields.forEach((field) => {
+        cy.dataCy(field).should('have.attr', 'aria-invalid', 'true')
+      })
+    })
+    cy.dataCy('pickupCoffee-form').should('exist')
+    cy.get('.toaster-success').should('not.exist')
+  })
+})
