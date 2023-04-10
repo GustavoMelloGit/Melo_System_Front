c683dacad9503f1159332181e0a2ae1a3f108c75
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-04-10 19:39:53 -0300

    refactor(CoffeeAccount): remove unused useKgPerBag store and update CoffeeAccountTableRow to remove kgPerBag dependency
    refactor(coffee-account): remove unused code and form for kgPerBag input in CoffeeAccountView component
    
    refactor(useView.tsx): remove unused handleSubmitKgPerBag function and useKgPerBag hook import statement

--- src/domain/client/view/Client/CoffeeAccount/view/List/index.tsx
+++ src/domain/client/view/Client/CoffeeAccount/view/List/index.tsx
@@ -1,35 +1,12 @@
 import { Stack } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
-import RHFField from '../../../../../../../shared/components/inputs/RHFField'
 import CoffeeAccountTable from '../../components/List'
-import { INITIAL_KG_PER_BAG } from '../../constants'
 import useCoffeeAccountView from './useView'
 
 export default function CoffeeAccountView(): JSX.Element {
-  const { data, isLoading, total, handleSubmitKgPerBag } = useCoffeeAccountView()
-  const { register, handleSubmit } = useForm<{ kgPerBag: number }>({
-    defaultValues: {
-      kgPerBag: INITIAL_KG_PER_BAG,
-    },
-  })
+  const { data, isLoading, total } = useCoffeeAccountView()
 
   return (
     <Stack spacing={4}>
-      <form
-        onSubmit={handleSubmit((values) => {
-          handleSubmitKgPerBag(values.kgPerBag)
-        })}
-      >
-        <RHFField
-          name='kgPerBag'
-          register={register}
-          label='Kg por saca'
-          type='number'
-          inputMode='numeric'
-          min={0}
-          step={0.1}
-        />
-      </form>
       <CoffeeAccountTable data={data} isLoading={isLoading} totalLength={total} />
     </Stack>
   )
