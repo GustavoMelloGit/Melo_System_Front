8eaa7606b284978e134696b8bb9f9f1f0608f321
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-04-17 18:52:51 -0300

    refactor(coffee.ts): remove utilization field from CoffeeDetails and add EscolhaDetails type refactor(coffee.ts): add DespolpadoDetails type refactor(sheet.ts): update coffeeDetails field type to accept DespolpadoDetails and EscolhaDetails types

--- src/domain/coffee/types/model/coffee.ts
+++ src/domain/coffee/types/model/coffee.ts
@@ -5,9 +5,14 @@ export type CoffeeDetails = {
   drilled: number
   foulness: number
   description: string
-  utilization: number
   type: CoffeeDetailsTypes
 }
+
+export type EscolhaDetails = Omit<CoffeeDetails, 'type'> & {
+  utilization: number
+}
+
+export type DespolpadoDetails = Omit<CoffeeDetails, 'type'>
 
 export type CoffeeTypes = 'bica_corrida' | 'despolpado' | 'conilon' | 'escolha'
 
@@ -27,7 +32,6 @@ export type CoffeeDetailsTypes =
   | 'duro_riado_rio'
   | 'duro_riado'
   | 'duro'
-  | 'despolpado'
 
 export const CoffeeDetailsTypesEnum: Record<CoffeeDetailsTypes, string> = {
   rio_velho: 'Rio Velho',
@@ -38,5 +42,4 @@ export const CoffeeDetailsTypesEnum: Record<CoffeeDetailsTypes, string> = {
   duro_riado_rio: 'Duro Riado Rio',
   duro_riado: 'Duro Riado',
   duro: 'Duro',
-  despolpado: 'Despolpado',
 }
