d512bec019dca6a0219749ea4f308b1c2be01e8b
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-04-18 19:04:58 -0300

    refactor(Client/Details): remove unused handleChangeTab function feat(Client/Details): add Link import from react-router-dom
    refactor(ClientDetails): replace Tabs onChange prop with Link to query params for tab selection
    feat(ClientDetails): add Link to query params for each tab in TabList
    
    refactor(useView.tsx): remove handleChangeTab function from useClientDetailsView hook

--- src/domain/client/view/Client/Details/useView.tsx
+++ src/domain/client/view/Client/Details/useView.tsx
@@ -7,18 +7,13 @@ import { type ClientModel } from '../../../types/model/Client'
 export default function useClientDetailsView(): UseClientDetailsView {
   const { uuid } = useParams()
   const { data, isLoading, mutate } = getClientService(uuid ?? '')
-  const { handleAddParam, getParam } = useURLSearchParams()
+  const { getParam } = useURLSearchParams()
   const currentTab = Number(getParam('tab')) ?? 0
-
-  const handleChangeTab = (index: number): void => {
-    handleAddParam('tab', index)
-  }
 
   return {
     client: data,
     isLoading,
     mutate,
-    handleChangeTab,
     currentTab,
   }
 }
@@ -27,6 +22,5 @@ type UseClientDetailsView = {
   client: ClientModel | undefined
   isLoading: boolean
   mutate: KeyedMutator<ClientModel> | undefined
-  handleChangeTab: (index: number) => void
   currentTab: number
 }
