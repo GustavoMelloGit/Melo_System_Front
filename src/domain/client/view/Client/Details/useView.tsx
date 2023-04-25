f2b9b89f12bd95cca52384238ba6afe08c6d72df
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-04-25 20:41:10 -0300

    feat(ClientDetails): add button to open modal with client general information fix(GeneralInfo): change component to be a modal instead of a box refactor(Theme): change menu item labels to portuguese

--- src/domain/client/view/Client/Details/useView.tsx
+++ src/domain/client/view/Client/Details/useView.tsx
@@ -1,20 +1,29 @@
 import { useParams } from 'react-router-dom'
 import { type KeyedMutator } from 'swr'
+import { useModal } from '../../../../../shared/hooks/useModal'
 import useURLSearchParams from '../../../../../shared/hooks/useURLSearchParams'
 import { getClientService } from '../../../service'
 import { type ClientModel } from '../../../types/model/Client'
 
 export default function useClientDetailsView(): UseClientDetailsView {
+  const openModal = useModal((state) => state.openModal)
   const { uuid } = useParams()
   const { data, isLoading, mutate } = getClientService(uuid ?? '')
   const { getParam } = useURLSearchParams()
   const currentTab = Number(getParam('tab')) ?? 0
+
+  async function openClientInfoModal(): Promise<void> {
+    if (!data) return
+    const GeneralInfo = (await import('../GeneralInfo')).default
+    openModal(<GeneralInfo client={data} />)
+  }
 
   return {
     client: data,
     isLoading,
     mutate,
     currentTab,
+    openClientInfoModal,
   }
 }
 
@@ -23,4 +32,5 @@ type UseClientDetailsView = {
   isLoading: boolean
   mutate: KeyedMutator<ClientModel> | undefined
   currentTab: number
+  openClientInfoModal: () => void
 }
