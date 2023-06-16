22adfdbb1fd7eae0ade45947c85c4d3d722d3297
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-06-15 22:23:23 -0300

    style(SidebarFooter.tsx): use CSS environment variable to set padding-bottom to avoid overlapping with device's safe area

--- src/shared/components/layout/Sidebar/components/SidebarFooter.tsx
+++ src/shared/components/layout/Sidebar/components/SidebarFooter.tsx
@@ -4,7 +4,7 @@ import SidebarSettings from './Settings'
 
 export default function SidebarFooter(): JSX.Element {
   return (
-    <Flex justify='center' align='center' as='footer' pb={4} gap={2}>
+    <Flex justify='center' align='center' as='footer' pb='env(safe-area-inset-bottom)' gap={2}>
       <SidebarSettings />
       <LockScreenButton />
     </Flex>
