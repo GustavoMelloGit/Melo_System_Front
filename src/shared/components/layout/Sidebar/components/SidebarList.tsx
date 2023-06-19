6e0d00710fb07382d47dfb142ab8234981dc848c
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-06-19 20:03:18 -0300

    chore: comment transfer to deploy

--- src/shared/components/layout/Sidebar/components/SidebarList.tsx
+++ src/shared/components/layout/Sidebar/components/SidebarList.tsx
@@ -1,7 +1,6 @@
 import { Box, List, VStack } from '@chakra-ui/react'
 import { BiUser } from 'react-icons/bi'
 import { BsTruck } from 'react-icons/bs'
-import { CgArrowsExchange } from 'react-icons/cg'
 import { MdOutlineMonitorWeight } from 'react-icons/md'
 import { TbPlant, TbTruckLoading } from 'react-icons/tb'
 import { useLocation } from 'react-router-dom'
@@ -38,10 +37,10 @@ const listItem: Record<
     label: 'Cafés a Buscar',
     icon: <BsTruck />,
   },
-  [Routes.transfer]: {
-    label: 'Transferência',
-    icon: <CgArrowsExchange size={24} />,
-  },
+  // [Routes.transfer]: {
+  //   label: 'Transferência',
+  //   icon: <CgArrowsExchange size={24} />,
+  // },
 }
 
 const customPaths: Record<string, string> = {
