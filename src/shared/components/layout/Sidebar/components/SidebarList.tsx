cec4b7f09fa4f3d242fb78d39e3a4e14c8934ecc
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-06-19 19:18:03 -0300

    feat(client): add TransferCurrencyView to client routes feat(client): add transfer route to routes feat(sidebar): add TransferCurrencyView to SidebarList style(sidebar): center icon in SidebarListItem

--- src/shared/components/layout/Sidebar/components/SidebarList.tsx
+++ src/shared/components/layout/Sidebar/components/SidebarList.tsx
@@ -1,6 +1,7 @@
 import { Box, List, VStack } from '@chakra-ui/react'
 import { BiUser } from 'react-icons/bi'
 import { BsTruck } from 'react-icons/bs'
+import { CgArrowsExchange } from 'react-icons/cg'
 import { MdOutlineMonitorWeight } from 'react-icons/md'
 import { TbPlant, TbTruckLoading } from 'react-icons/tb'
 import { useLocation } from 'react-router-dom'
@@ -36,6 +37,10 @@ const listItem: Record<
   [Routes.coffeePickups]: {
     label: 'Cafés a Buscar',
     icon: <BsTruck />,
+  },
+  [Routes.transfer]: {
+    label: 'Transferência',
+    icon: <CgArrowsExchange size={24} />,
   },
 }
 
