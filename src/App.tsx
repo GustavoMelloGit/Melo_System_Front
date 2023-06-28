24139658fe7b70dfa90004ad3544cdcc1631ff24
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-06-28 17:54:27 -0300

    feat(App.tsx): add support for screen lock when user is idle for 5 minutes fix(LockScreenView.tsx): add 'autoComplete' attribute to password input field to disable autofill feat(useIdle.tsx): create custom hook to track user idle state and trigger lock screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Fragment, lazy } from 'react'
 import { HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
+import { shallow } from 'zustand/shallow'
 import { AuthProvider } from './domain/auth/context/AuthContext'
 import './lib/config/firebase'
 import router from './lib/routes/router'
@@ -12,10 +13,19 @@ import ModalManager from './shared/components/ModalManager'
 import ProviderComposer from './shared/components/ProviderComposer'
 import Suspense from './shared/components/Suspense'
 import LayoutProvider from './shared/contexts/LayoutContext'
+import useIdle from './shared/hooks/useIdle'
 const LockScreenView = lazy(async () => import('./shared/components/layout/Content/LockScreenView'))
 
 function App(): JSX.Element {
-  const isLocked = useScreenProtectionStore((state) => state.isLocked)
+  const [isLocked, lock] = useScreenProtectionStore(
+    (state) => [state.isLocked, state.lock],
+    shallow,
+  )
+  const { isIdle } = useIdle(5 * 60 * 1000)
+
+  if (isIdle && !isLocked) {
+    lock()
+  }
 
   return (
     <ChakraProvider theme={theme} colorModeManager={themeManager}>
---
+++ src/shared/hooks/useIdle.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef, useState } from 'react'
+
+export default function useIdle(delay: number = 10000): UseIdle {
+  const [isIdle, setIsIdle] = useState<boolean>(false)
+  // create a new reference to track timer
+  const timeoutId = useRef<NodeJS.Timeout>()
+
+  // assign and remove the listeners
+  useEffect(() => {
+    setup()
+
+    return () => {
+      cleanUp()
+    }
+  })
+
+  const startTimer = (): void => {
+    timeoutId.current = setTimeout(goInactive, delay)
+  }
+
+  const resetTimer = (): void => {
+    clearTimeout(timeoutId.current)
+    goActive()
+  }
+
+  const goInactive = (): void => {
+    setIsIdle(true)
+  }
+
+  const goActive = (): void => {
+    setIsIdle(false)
+    startTimer()
+  }
+
+  const setup = (): void => {
+    document.addEventListener('mousemove', resetTimer, false)
+    document.addEventListener('mousedown', resetTimer, false)
+    document.addEventListener('keypress', resetTimer, false)
+    document.addEventListener('DOMMouseScroll', resetTimer, false)
+    document.addEventListener('mousewheel', resetTimer, false)
+    document.addEventListener('touchmove', resetTimer, false)
+    document.addEventListener('MSPointerMove', resetTimer, false)
+    window.addEventListener('blur', startTimer, false)
+    window.addEventListener('focus', resetTimer, false)
+  }
+
+  const cleanUp = (): void => {
+    document.removeEventListener('mousemove', resetTimer)
+    document.removeEventListener('mousedown', resetTimer)
+    document.removeEventListener('keypress', resetTimer)
+    document.removeEventListener('DOMMouseScroll', resetTimer)
+    document.removeEventListener('mousewheel', resetTimer)
+    document.removeEventListener('touchmove', resetTimer)
+    document.removeEventListener('MSPointerMove', resetTimer)
+    window.removeEventListener('blur', startTimer)
+    window.removeEventListener('focus', resetTimer)
+
+    clearTimeout(timeoutId.current)
+  }
+
+  return {
+    isIdle,
+  }
+}
+
+type UseIdle = {
+  isIdle: boolean
+}
