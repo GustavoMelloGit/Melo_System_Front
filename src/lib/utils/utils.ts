fcebf32e42de514911ad055dd92d6f92501cedb0
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-08-26 11:30:19 -0300

    refactor(utils.ts): remove unused pluralize function
    The pluralize function was not being used in the codebase, so it was removed to improve code cleanliness and reduce unnecessary code.

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -27,17 +27,6 @@ export function removeEmptyObjects(obj: Record<string, any>): Record<string, any
 }
 
 /**
- * Pluralize a word given a number.
- * @param word The word to pluralize.
- * @param count The number to use to pluralize the word.
- * @param suffix The suffix to use when pluralizing the word.
- * @returns the pluralized word.
- */
-export function pluralize(word: string, count: number, suffix = 's'): string {
-  return count === 1 ? word : `${word}${suffix}`
-}
-
-/**
  * Returns default sort search params for given sort field and order.
  * @param sortBy The field to sort by.
  * @param sortOrder The order to sort by.
