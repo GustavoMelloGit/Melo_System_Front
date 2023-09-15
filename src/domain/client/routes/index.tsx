4c394711476c0d0051e5eaca087a37245d05d676
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-09-15 18:36:45 -0300

    refactor: :truck: move client files to client folder

--- src/domain/client/routes/index.tsx
+++ src/domain/client/routes/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { type RouteObject } from 'react-router-dom'
 import { Routes } from '../../../lib/routes'
 const ClientsListView = React.lazy(async () => import('../view/Clients/List'))
-const CreateClientsView = React.lazy(async () => import('../view/Clients/Create'))
-const UpdateClientsView = React.lazy(async () => import('../view/Clients/Update'))
+const CreateClientsView = React.lazy(async () => import('../view/Client/Create'))
+const UpdateClientsView = React.lazy(async () => import('../view/Client/Update'))
 const ClientDetailsView = React.lazy(async () => import('../view/Client/Details'))
 const TransferCurrencyView = React.lazy(async () => import('../view/Client/Transfer'))
 const ClientSheetsView = React.lazy(async () => import('../view/Client/Sheets'))
