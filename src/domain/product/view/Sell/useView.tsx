c47f35d3f4e586a3faa2685f04aeb9cad99f08df
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-11 18:32:52 -0300

    fix(product): add client check before selling products feat(product): display client link when selling products refactor(product): remove unused code and add error handling in sell function

--- src/domain/product/view/Sell/useView.tsx
+++ src/domain/product/view/Sell/useView.tsx
@@ -1,32 +1,48 @@
-import { useModal } from '../../../../shared/hooks/useModal'
+import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { Routes } from '../../../../lib/routes'
+import useURLSearchParams from '../../../../shared/hooks/useURLSearchParams'
+import { sellFertilizerService } from '../../../client/view/Client/Accounts/Product/services/post'
 import { type SellProductFormValues } from '../../components/Sell/Form/types'
 
 const useSellProductView = (): UseSellFertilizerView => {
-  const closeModal = useModal((state) => state.closeModal)
+  const { allSearchParams } = useURLSearchParams()
+  const { client } = allSearchParams
+  const navigate = useNavigate()
 
   async function handleSellFertilizer(values: SellProductFormValues): Promise<void> {
-    console.log(values)
-    // const { client, products } = values
-    // const { error } = await sellFertilizerService({
-    //   clientId,
-    //   ...formValues,
-    // })
-    // if (error) {
-    //   toast.error(error)
-    //   return
-    // }
-    // toast.success('Fertilizante vendido com sucesso')
-    // closeModal()
+    if (!client) return
+    try {
+      await Promise.all(
+        values.products.map(async (product) =>
+          sellFertilizerService({
+            clientId: client,
+            bags: product.quantity,
+            fertilizerId: product.productId,
+            pricePerBag: product.price,
+            brook: product.brook,
+            complement: product.complement,
+            deliveryDate: product.deliveryDate,
+          }),
+        ),
+      )
+      toast.success('Produtos vendidos com sucesso!')
+      navigate(Routes.clientPage(client))
+    } catch (error) {
+      toast.error(
+        'Ocorreu um erro na venda dos produtos. Verifique na conta do cliente quais produtos foram vendidos com sucesso.',
+      )
+    }
   }
 
   return {
-    closeModal,
     handleSellFertilizer,
+    client,
   }
 }
 type UseSellFertilizerView = {
-  closeModal: () => void
   handleSellFertilizer: (values: SellProductFormValues) => Promise<void>
+  client: string | undefined
 }
 
 export default useSellProductView
