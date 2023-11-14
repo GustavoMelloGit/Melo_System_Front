ca60ba46bd676d3fb4b2ebce4fd6d66f92bbac72
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-14 20:48:37 -0300

    feat(client): add getTransactionsFromClientService function
    This commit adds a new file `getTransactionsFromClientService.ts` under the `src/domain/client/service` directory. The file contains a function `getTransactionsFromClientService` that retrieves transactions from a client based on the provided type and client ID. The function uses the `useFetch` hook from the `useFetch.ts` file and makes a request to the `/metrics/findAllTransactionsFromClient` endpoint with the specified type and client ID as query parameters. The function returns a `SWRServiceResponse` object containing the response data.
    
    The purpose of this change is to provide a convenient way to fetch transactions from a client based on the specified type and client ID. This function can be used in other parts of the codebase to retrieve and display client transactions.

---
+++ src/domain/client/service/getTransactionsFromClientService.ts
@@ -0,0 +1,37 @@
+import { type SWRConfiguration } from 'swr'
+import useFetch from '../../../shared/hooks/useFetch'
+import { type SWRServiceResponse } from '../../../shared/types/service/SWRServiceResponse'
+import {
+  type CoffeeTransactionModel,
+  type CurrencyTransactionModel,
+  type EscolhaTransactionModel,
+  type FertilizerTransactionModel,
+  type SacariaTransactionModel,
+} from '../types/model/Transaction'
+
+type Type = 'currency' | 'coffee' | 'escolha' | 'bags' | 'fertilizer'
+
+type ResponseByType<T extends Type> = T extends 'currency'
+  ? CurrencyTransactionModel
+  : T extends 'coffee'
+  ? CoffeeTransactionModel
+  : T extends 'escolha'
+  ? EscolhaTransactionModel
+  : T extends 'bags'
+  ? SacariaTransactionModel
+  : T extends 'fertilizer'
+  ? FertilizerTransactionModel
+  : unknown
+
+export function getTransactionsFromClientService<T extends Type>(
+  type: T,
+  clientId: string,
+  config?: SWRConfiguration,
+): SWRServiceResponse<ResponseByType<T>> {
+  const response = useFetch(
+    `/metrics/findAllTransactionsFromClient?type=${type}&clientId=${clientId}`,
+    config,
+  )
+
+  return response
+}
