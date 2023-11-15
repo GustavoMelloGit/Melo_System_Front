d951b47a165a13d5f97dfd4795ac7146cb9150c9
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-11-15 09:22:59 -0300

    fix(client): fix DownloadButton component to properly fetch and update data feat(client): add event listener to refetch data when transaction is created

--- src/domain/client/view/Client/Accounts/Checking/components/Template/DownloadButton.tsx
+++ src/domain/client/view/Client/Accounts/Checking/components/Template/DownloadButton.tsx
@@ -3,21 +3,34 @@ import { useCallback, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import DownloadButton from '../../../../../../../../shared/components/buttons/DownloadButton'
 import { getTransactionsFromClientService } from '../../../../../../service/getTransactionsFromClientService'
+import { CheckingAccountEmitter } from '../../events/CheckingAccountEmitter'
 import CoffeePriceMetricsTemplate from './Template'
 
 export default function DownloadCheckingAccountButton(): JSX.Element {
   const { uuid } = useParams<'uuid'>()
-  const { data } = getTransactionsFromClientService('currency', uuid ?? '')
+  const { data, mutate } = getTransactionsFromClientService('currency', uuid ?? '')
   const [instance, updateInstance] = usePDF({
     document: <CoffeePriceMetricsTemplate data={data ?? []} />,
   })
+
   const updatePdfInstance = useCallback(async () => {
     updateInstance(<CoffeePriceMetricsTemplate data={data ?? []} />)
   }, [data, updateInstance])
 
+  const refetchData = useCallback(async () => {
+    await mutate()
+  }, [mutate])
+
   useEffect(() => {
     void updatePdfInstance()
   }, [updatePdfInstance])
+
+  useEffect(() => {
+    CheckingAccountEmitter.on('transactionCreated', refetchData)
+    return () => {
+      CheckingAccountEmitter.off('transactionCreated', refetchData)
+    }
+  }, [refetchData])
 
   return (
     <DownloadButton
