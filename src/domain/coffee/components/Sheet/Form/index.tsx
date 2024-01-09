c195a5402e3400b8a6c66f99cadd70290b0f18cf
Gustavo Mello <gugamello2014@yahoo.com>
2024-01-09 19:43:36 -0300

    refactor: :recycle: improve validation, fix default values errors, improve code structure

--- src/domain/coffee/components/Sheet/Form/index.tsx
+++ src/domain/coffee/components/Sheet/Form/index.tsx
@@ -3,25 +3,52 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { useEffect } from 'react'
 import { useForm, type Path } from 'react-hook-form'
 import * as yup from 'yup'
+import { CoffeeTypeHasBebida } from '../../../../../lib/constants/coffee'
 import { validationErrors } from '../../../../../lib/errors'
 import { dateInputToApiDate } from '../../../../../lib/utils/date'
+import { type CoffeeTypes } from '../../../types/model/coffee'
 import { type SheetFormValues } from '../../../types/model/sheet'
 import SheetFormCoffeeDetails from './CoffeeDetails'
 import SheetFormLines from './Lines'
 import SheetFormSheetDetails from './SheetDetails'
 
-type Props = {
-  initialValues: SheetFormValues
-  onSubmit?: (values: SheetFormValues) => Promise<void>
-  variant?: 'create' | 'edit' | 'view'
+const emptyInitialValues: SheetFormValues = {
+  courier: '',
+  clientId: '',
+  clientName: '',
+  number: 0,
+  isDraft: false,
+  lines: [{ bags: 0, weight: 0 }],
+  weighingDate: new Date().toISOString().split('T')[0],
+  coffeeDetails: {
+    coffeeType: 'bica_corrida',
+    bebida: 'duro',
+    description: '',
+    drilled: 0,
+    foulness: 0,
+    moisture: 0,
+    picking: 0,
+    sieve: 0,
+    utilization: 0,
+  },
 }
-export default function SheetForm({
-  initialValues,
-  onSubmit,
-  variant = 'create',
-}: Props): JSX.Element {
+
+type Props =
+  | {
+      variant: 'create' | 'edit'
+      initialValues?: SheetFormValues
+      onSubmit: (values: SheetFormValues) => Promise<void>
+    }
+  | {
+      variant: 'view'
+      initialValues: SheetFormValues
+      onSubmit?: never
+    }
+
+export default function SheetForm({ initialValues, onSubmit, variant }: Props): JSX.Element {
   const { register, handleSubmit, formState, control, setValue, reset } = useForm<SheetFormValues>({
     resolver: yupResolver(validationSchema),
+    defaultValues: initialValues ?? emptyInitialValues,
   })
 
   function isDisabled(fieldName: Path<SheetFormValues>): boolean {
@@ -35,6 +62,7 @@ export default function SheetForm({
     }
     return false
   }
+
   useEffect(() => {
     if (initialValues) {
       reset(initialValues)
@@ -48,27 +76,8 @@ export default function SheetForm({
           ...values,
           weighingDate: dateInputToApiDate(weighingDate),
         })
-        const emptyInitialValues: SheetFormValues = {
-          courier: '',
-          clientId: '',
-          clientName: '',
-          number: values.number + 1,
-          isDraft: false,
-          lines: [{ bags: 0, weight: 0 }],
-          weighingDate: new Date().toISOString().split('T')[0],
-          coffeeDetails: {
-            coffeeType: 'bica_corrida',
-            bebida: 'duro',
-            description: '',
-            drilled: 0,
-            foulness: 0,
-            moisture: 0,
-            picking: 0,
-            sieve: 0,
-            utilization: 0,
-          },
-        }
-        reset(emptyInitialValues)
+
+        reset({ ...emptyInitialValues, number: values.number + 1 })
       })}
     >
       <Card>
@@ -138,26 +147,30 @@ const validationSchema = yup.object().shape({
   ),
   coffeeDetails: yup.object().shape({
     coffeeType: yup.string().required(validationErrors.coffeeTypeIsRequired),
+    bebida: yup.string().when('coffeeType', ([coffeeType], schema) => {
+      const hasBebida = CoffeeTypeHasBebida.includes(coffeeType as CoffeeTypes)
+      return hasBebida ? schema.required(validationErrors.bebidaIsRequired) : schema.notRequired()
+    }),
     moisture: yup
-      .string()
-      .min(0, validationErrors.minIsInvalid(0))
-      .max(100, validationErrors.maxIsInvalid(100)),
+      .number()
+      .min(0, validationErrors.minValueIsInvalid(0))
+      .max(100, validationErrors.maxValueIsInvalid(100)),
     foulness: yup
-      .string()
-      .min(0, validationErrors.minIsInvalid(0))
-      .max(100, validationErrors.maxIsInvalid(100)),
+      .number()
+      .min(0, validationErrors.minValueIsInvalid(0))
+      .max(100, validationErrors.maxValueIsInvalid(100)),
     sieve: yup
-      .string()
-      .min(0, validationErrors.minIsInvalid(0))
-      .max(100, validationErrors.maxIsInvalid(100)),
+      .number()
+      .min(0, validationErrors.minValueIsInvalid(0))
+      .max(100, validationErrors.maxValueIsInvalid(100)),
     picking: yup
-      .string()
-      .min(0, validationErrors.minIsInvalid(0))
-      .max(100, validationErrors.maxIsInvalid(100)),
+      .number()
+      .min(0, validationErrors.minValueIsInvalid(0))
+      .max(100, validationErrors.maxValueIsInvalid(100)),
     drilled: yup
-      .string()
-      .min(0, validationErrors.minIsInvalid(0))
-      .max(100, validationErrors.maxIsInvalid(100)),
+      .number()
+      .min(0, validationErrors.minValueIsInvalid(0))
+      .max(100, validationErrors.maxValueIsInvalid(100)),
     description: yup.string(),
   }),
 })
